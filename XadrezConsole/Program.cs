3d1feec820a64473630cc13eab755a0dd317405f
Alyson Ramos <ramos.alysonfarias@gmail.com>
2021-11-24 15:57:34 -0300

    Added colors to the boardpieces

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -8,28 +8,28 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Position P;
+            try
+            {
+                Position P;
 
-            //    P = new Position(3, 5);
-            //    Console.WriteLine("Position : " + P);
+                P = new Position(3, 5);
+                Console.WriteLine("Position : " + P);
 
-            //    table tab = new table(8, 8);
-            //    tab.insertBoardPiece(new Tower(tab, Color.Black), new Position(0, 0));
-            //    tab.insertBoardPiece(new Tower(tab, Color.Black), new Position(1, 3));
-            //    tab.insertBoardPiece(new King(tab, Color.Black), new Position(4, 9));
-            //    Screen.printTable(tab);
-            //}
-            //catch (ExceptionTable e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
-            XadrezPosition P;
-            P = new XadrezPosition('a', 1);
-            table tab = new table(8, 8);
-            Console.WriteLine("Position : " + P.toPositition());
-            Screen.printTable(tab);
+                table tab = new table(8, 8);
+                tab.insertBoardPiece(new Tower(tab, Color.Black), new Position(0, 0));
+                tab.insertBoardPiece(new Tower(tab, Color.Black), new Position(1, 3));
+                tab.insertBoardPiece(new Tower(tab, Color.White), new Position(0, 3));
+                Screen.printTable(tab);
+            }
+            catch (ExceptionTable e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //XadrezPosition P;
+            //P = new XadrezPosition('a', 1);
+            //table tab = new table(8, 8);
+            //Console.WriteLine("Position : " + P.toPositition());
+            //Screen.printTable(tab);
         }
     }
 }
--- XadrezConsole/Screen.cs
+++ XadrezConsole/Screen.cs
@@ -14,6 +14,8 @@ namespace XadrezConsole
         {
             for (int i = 0; i < tab.lines; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < tab.columns; j++)
                 {
                     if(tab.boardPiece(i, j) == null)
@@ -22,10 +24,26 @@ namespace XadrezConsole
                     }
                     else
                     {
-                        Console.Write(tab.boardPiece(i,j) + " ");
+                        printBoardPiece(tab.boardPiece(i,j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void printBoardPiece(BoardPiece boardPiece)
+        {
+            if(boardPiece.color == Color.White)
+            {
+                Console.Write(boardPiece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(boardPiece);
+                Console.ForegroundColor = aux;
             }
         }
     }
