0e3fa7f3bbdbed0a092df99c8ce1408c7899fa67
Alyson Ramos <ramos.alysonfarias@gmail.com>
2021-11-24 15:37:46 -0300

    Created Xadrez Position

---
+++ XadrezConsole/Xadrez/XadrezPosition.cs
@@ -0,0 +1,24 @@
+﻿using Table;
+namespace xadrez
+{
+    class XadrezPosition
+    {
+        public char column { get; set; }
+        public int line { get; set; }
+
+        public XadrezPosition (char column, int line)
+        {
+            this.column = column;
+            this.line = line;
+        }
+        public override string ToString()
+        {
+            return this.column + ", " + this.line;
+        }
+
+        public Position toPositition()
+        {
+            return new Position(8 - line, column - 'a');
+        }
+    }
+}
